9fdc4422c0870a8bd848b4cf63beda9bbf9f0280
Nunwan <perso@nunwan.fr>
2021-03-09 10:56:42 +0100

    Refactor map as a struct with methods

--- src/map.rs
+++ src/map.rs
@@ -2,6 +2,13 @@ use std::cmp::{max, min};
 
 use rltk::RandomNumberGenerator;
 
+pub struct Map {
+    pub map: Vec<TileType>,
+    pub rooms: Vec<Rect>,
+    pub height: i32,
+    pub width: i32,
+}
+
 #[derive(PartialEq, Clone, Copy)]
 pub enum TileType {
     Wall,
@@ -35,76 +42,81 @@ impl Rect {
         ((self.x1 + self.x2) / 2, (self.y1 + self.y2) / 2)
     }
 }
-
-pub fn xy_idx(x: i32, y: i32, _h: u32, w: u32) -> usize {
-    assert!(x >= 0 && y >= 0);
-    (y * w as i32 + x) as usize
-}
-
-fn apply_room_to_map(room: &Rect, map: &mut [TileType], height: u32, width: u32) {
-    for y in room.y1 + 1..=room.y2 {
-        for x in room.x1 + 1..=room.x2 {
-            map[xy_idx(x, y, height, width)] = TileType::Floor;
-        }
+impl Map {
+    pub fn xy_idx(&self, x: i32, y: i32) -> usize {
+        assert!(x >= 0 && y >= 0);
+        (y * self.width + x) as usize
     }
-}
 
-fn apply_horizontal_tunnel(map: &mut [TileType], x1: i32, x2: i32, y: i32, h: u32, w: u32) {
-    for x in min(x1, x2)..=max(x1, x2) {
-        let idx = xy_idx(x, y, h, w);
-        if idx > 0 && idx < (h * w) as usize {
-            map[idx as usize] = TileType::Floor;
-        }
-    }
-}
-
-fn apply_vertical_tunnel(map: &mut [TileType], y1: i32, y2: i32, x: i32, h: u32, w: u32) {
-    for y in min(y1, y2)..=max(y1, y2) {
-        let idx = xy_idx(x, y, h, w);
-        if idx > 0 && idx < (h * w) as usize {
-            map[idx as usize] = TileType::Floor;
-        }
-    }
-}
-
-/// Map with solid boundaries
-pub fn new_map_rooms_and_corridors(width: u32, height: u32) -> (Vec<Rect>, Vec<TileType>) {
-    let mut map = vec![TileType::Wall; (height * width) as usize];
-    let mut rooms: Vec<Rect> = Vec::new();
-    const MAX_ROOMS: i32 = 30;
-    const MIN_SIZE: i32 = 6;
-    const MAX_SIZE: i32 = 10;
-
-    let mut rng = RandomNumberGenerator::new();
-    for _ in 0..MAX_ROOMS {
-        let w = rng.range(MIN_SIZE, MAX_SIZE);
-        let h = rng.range(MIN_SIZE, MAX_SIZE);
-        let x = rng.roll_dice(1, width as i32 - w - 1) - 1;
-        let y = rng.roll_dice(1, height as i32 - h - 1) - 1;
-        let new_room = Rect::new(x, y, w, h);
-        let mut ok = true;
-        for other_room in rooms.iter() {
-            if new_room.intersect(other_room) {
-                ok = false;
+    fn apply_room_to_map(&mut self, room: &Rect) {
+        for y in room.y1 + 1..=room.y2 {
+            for x in room.x1 + 1..=room.x2 {
+                let idx = self.xy_idx(x, y);
+                self.map[idx] = TileType::Floor;
             }
         }
-        if ok {
-            apply_room_to_map(&new_room, &mut map, height, width);
-            if !rooms.is_empty() {
-                let (new_x, new_y) = new_room.center();
-                let (prev_x, prev_y) = rooms[rooms.len() - 1].center();
-                if rng.range(0, 2) == 1 {
-                    apply_horizontal_tunnel(&mut map, prev_x, new_x, prev_y, height, width);
-                    apply_vertical_tunnel(&mut map, prev_y, new_y, new_x, height, width);
-                } else {
-                    apply_vertical_tunnel(&mut map, prev_y, new_y, prev_x, height, width);
-                    apply_horizontal_tunnel(&mut map, prev_x, new_x, new_y, height, width);
+    }
+
+    fn apply_horizontal_tunnel(&mut self, x1: i32, x2: i32, y: i32) {
+        for x in min(x1, x2)..=max(x1, x2) {
+            let idx = self.xy_idx(x, y);
+            if idx > 0 && idx < (self.height * self.width) as usize {
+                self.map[idx as usize] = TileType::Floor;
+            }
+        }
+    }
+
+    fn apply_vertical_tunnel(&mut self, y1: i32, y2: i32, x: i32) {
+        for y in min(y1, y2)..=max(y1, y2) {
+            let idx = self.xy_idx(x, y);
+            if idx > 0 && idx < (self.height * self.width) as usize {
+                self.map[idx as usize] = TileType::Floor;
+            }
+        }
+    }
+
+    /// Map with solid boundaries
+    pub fn new_map_rooms_and_corridors(width: i32, height: i32) -> Map {
+        let mut map = Map {
+            map: vec![TileType::Wall; (width * height) as usize],
+            rooms: Vec::new(),
+            width,
+            height,
+        };
+        const MAX_ROOMS: i32 = 30;
+        const MIN_SIZE: i32 = 6;
+        const MAX_SIZE: i32 = 10;
+
+        let mut rng = RandomNumberGenerator::new();
+        for _ in 0..MAX_ROOMS {
+            let w = rng.range(MIN_SIZE, MAX_SIZE);
+            let h = rng.range(MIN_SIZE, MAX_SIZE);
+            let x = rng.roll_dice(1, width as i32 - w - 1) - 1;
+            let y = rng.roll_dice(1, height as i32 - h - 1) - 1;
+            let new_room = Rect::new(x, y, w, h);
+            let mut ok = true;
+            for other_room in map.rooms.iter() {
+                if new_room.intersect(other_room) {
+                    ok = false;
                 }
             }
+            if ok {
+                map.apply_room_to_map(&new_room);
+                if !map.rooms.is_empty() {
+                    let (new_x, new_y) = new_room.center();
+                    let (prev_x, prev_y) = map.rooms[map.rooms.len() - 1].center();
+                    if rng.range(0, 2) == 1 {
+                        map.apply_horizontal_tunnel(prev_x, new_x, prev_y);
+                        map.apply_vertical_tunnel(prev_y, new_y, new_x);
+                    } else {
+                        map.apply_vertical_tunnel(prev_y, new_y, prev_x);
+                        map.apply_horizontal_tunnel(prev_x, new_x, new_y);
+                    }
+                }
 
-            rooms.push(new_room);
+                map.rooms.push(new_room);
+            }
         }
+        map
     }
-
-    (rooms, map)
 }
--- src/map_gen.rs
+++ src/map_gen.rs
@@ -1,6 +1,6 @@
 use crate::{
     create_player,
-    map::{new_map_rooms_and_corridors, TileType},
+    map::{Map, TileType},
     Position, Renderable, RigidBody,
 };
 use rltk::RGB;
@@ -15,16 +15,11 @@ pub fn get_glyph_tile(tile: &TileType) -> rltk::FontCharType {
 }
 
 /// Given a vector of TileType, create all entities composing the map
-pub fn gen_map_from_tile(
-    map: &[TileType],
-    ecs: &mut World,
-    width: u32,
-    _height: u32,
-) -> Vec<Entity> {
+pub fn gen_map_from_tile(map: &Map, ecs: &mut World) -> Vec<Entity> {
     let mut x: i32 = 0;
     let mut y: i32 = 0;
     let mut map_entity: Vec<Entity> = Vec::new();
-    for tile in map.iter() {
+    for tile in map.map.iter() {
         let entity = ecs
             .create_entity()
             .with(Position { x, y })
@@ -42,7 +37,7 @@ pub fn gen_map_from_tile(
                 .expect("Impossible to create a rigid body for entity in map_gen");
         }
         x = x + 1;
-        if x as u32 == width {
+        if x == map.width {
             y = y + 1;
             x = 0;
         }
@@ -51,10 +46,10 @@ pub fn gen_map_from_tile(
 }
 
 /// Create a map and place a player inside
-pub fn gen_map(height: u32, width: u32, ecs: &mut World) {
-    let (rooms, map) = new_map_rooms_and_corridors(width, height);
-    let map_entity = gen_map_from_tile(&map, ecs, width, height);
-    let (player_x, player_y) = rooms[0].center();
+pub fn gen_map(height: i32, width: i32, ecs: &mut World) {
+    let map = Map::new_map_rooms_and_corridors(width, height);
+    let map_entity = gen_map_from_tile(&map, ecs);
+    let (player_x, player_y) = map.rooms[0].center();
     create_player(player_x, player_y, ecs);
     ecs.insert(map_entity);
 }
